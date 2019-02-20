81f5f91dad0279d2495b7e4245f124332eb086f0
PeterMeisrimel <Peter.Meisrimel@gmail.com>
2019-02-20 10:03:16 +0100

    changed communication pattern visualization to be more suitable for the multirate case

--- runtime_stats/comm_pattern_plots.py
+++ runtime_stats/comm_pattern_plots.py
@@ -52,19 +52,24 @@ def plot_comm_pattern(path):
         for i in range(msteps):
             log0_avg[i] /= parameters['times']
             log1_avg[i] /= parameters['times']
-        
+            
+            
+        spmmax = max(steps_per_macro_0, steps_per_macro_1)
         fig, ax = pl.subplots(2, figsize = (12, 10))
         for i in range(msteps):
-            ax[0].bar(range(steps_per_macro_0*i, steps_per_macro_0*(i+1)), log0_avg[i], color = 'blue')
-            ax[0].plot([steps_per_macro_0*(i+1)]*2, [0, 1], '--k')
-            ax[1].bar(range(steps_per_macro_1*i, steps_per_macro_1*(i+1)), log1_avg[i], color = 'red')
-            ax[1].plot([steps_per_macro_1*(i+1)]*2, [0, 1], '--k')
+            x0 = np.array(range(steps_per_macro_0*i, steps_per_macro_0*(i+1)))/spmmax/msteps
+            ax[0].bar(x0, log0_avg[i], color = 'blue', width = 1/(steps_per_macro_0*msteps)/1.5)
+            ax[0].plot([steps_per_macro_0*(i+1)/steps_per_macro_0/msteps]*2, [0, 1], '--k')
+            x1 = np.array(range(steps_per_macro_1*i, steps_per_macro_1*(i+1)))/spmmax/msteps
+            ax[1].bar(x1, log1_avg[i], color = 'red', width = 1/(steps_per_macro_1*msteps)/1.5)
+            ax[1].plot([steps_per_macro_1*(i+1)/steps_per_macro_1/msteps]*2, [0, 1], '--k')
             
         for i in range(2):
             ax[i].axhline(0, linestyle = '-', color = 'black')
             ax[i].axhline(1, linestyle = '--', color = 'black')
             ax[i].set_ylim([-0.1, 1.1])
-            ax[i].set_xlim([0, max(parameters['timesteps1'], parameters['timesteps2'])])
+            #ax[i].set_xlim([0, max(parameters['timesteps1'], parameters['timesteps2'])])
+            ax[i].set_xlim([0, 1])
             ax[i].set_ylabel('% use new data')
             ax[i].set_xlabel('timestep')
         ax[0].set_title('Processor 0: Dirichlet, WF tol = {}'.format(tol))
