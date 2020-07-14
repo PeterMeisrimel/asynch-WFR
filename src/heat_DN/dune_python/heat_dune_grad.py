small technical fix for dune heat flux computation and fixed some of the verification script and functions for consistency

src/heat_DN/dune_python/heat_dune_grad.py
@@ -32,7 +32,8 @@ class Problem_heat_D(Problem_heat):
         
         self.scheme = solutionScheme([self.A == self.b, *self.bcs], solver = 'cg')
         
-        self.flux_grad = expression2GF(self.unew.space.grid, ufl.grad(self.unew), self.unew.space.order)
+        self.normal = Constant((1., 0.))
+        self.flux_grad = expression2GF(self.unew.space.grid, ufl.dot(ufl.grad(self.unew), self.normal), self.unew.space.order)
         self.flux_f = lambda x: float(self.lam)*lineSample(x, [0., 0.], [0., 1.], self.NN)[1]
         
         self.ug_interp = interp1d(self.yy, np.zeros(self.NN))
@@ -41,17 +42,11 @@ class Problem_heat_D(Problem_heat):
     def get_u0(self): return self.get_flux()
 		
     def get_flux(self):
-#        gradUnew = expression2GF(self.unew.space.grid, ufl.grad(self.unew), self.unew.space.order)
-#        return -float(self.lam)*lineSample(gradUnew, [0., 0.], [0., 1.], self.NN)[1]
-        
-#        return -float(self.lam)*lineSample(self.flux_grad, [0., 0.], [0., 1.], self.NN)[1]
         return self.flux_f(self.flux_grad)
     
     def do_step(self, dt, ug):
         self.dt.value = dt
         
-#        intp = interp1d(self.yy, ug)
-#        self.u_gamma.interpolate(lambda x: intp(x[1]))
         self.ug_interp.y = ug
         self.u_gamma.interpolate(self.ug_f)
         
@@ -97,12 +92,6 @@ class Problem_heat_N(Problem_heat):
     def do_step(self, dt, flux_1, flux_2):
         self.dt.value = dt
         
-#        intp_flux = interp1d(self.yy, flux_1)
-#        self.fold.interpolate(lambda x: intp_flux(x[1]))
-        
-#        intp_flux = interp1d(self.yy, flux_2)
-#        self.fnew.interpolate(lambda x: intp_flux(x[1]))
-        
         self.f_old_interpol.y = flux_1
         self.fold.interpolate(self.f_old_f)
         
@@ -117,10 +106,10 @@ class Problem_heat_N(Problem_heat):
         dt = tf/n_steps
         for i, t in enumerate(np.linspace(0, tf, n_steps + 1)[:-1]):
             self.get_ex_flux(t) # ex flux now sits in unew
-            flux_old = -np.copy(self.get_u_gamma(self.unew))
+            flux_old = np.copy(self.get_u_gamma(self.unew))
             
             self.get_ex_flux(t + dt) # ex flux now sits in unew
-            flux_new = -np.copy(self.get_u_gamma(self.unew))
+            flux_new = np.copy(self.get_u_gamma(self.unew))
             
             self.do_step(dt, flux_old, flux_new)
 
@@ -156,7 +145,7 @@ if __name__ == '__main__':
 
 #    verify_with_monolithic(solve_WR = solver, k = 8, order = 1, **pp, savefig = savefig)
 #    verify_with_monolithic(solve_WR = solver, k = 8, order = 2, **pp, savefig = savefig)
-    
+#    
     from verification import verify_comb_error
     ## verify combined error, splitting + time int for decreasing dt
 #    verify_comb_error(solve_WR = solver, k = 8, order = 1, **pp, savefig = savefig)
@@ -172,7 +161,7 @@ if __name__ == '__main__':
     from verification import plot_theta
     pp = {'tf': 1., **get_parameters(), 'gridsize': 32, 'xa': -2, 'xb': 1}
 #    plot_theta(solve_WR = solver, savefig = savefig, order = 1, **pp)
-    plot_theta(solve_WR = solver, savefig = savefig, order = 2, **pp)
+#    plot_theta(solve_WR = solver, savefig = savefig, order = 2, **pp)
     
     ## verify time-integration order with itself
     from verification import verify_self_time

src/heat_DN/dune_python/verification_D_N.py
@@ -9,7 +9,6 @@ Created on Wed May 20 16:43:51 2020
 import numpy as np
 import pylab as pl
 from heat_dune import Problem_heat
-from scipy.interpolate import interp1d
 
 pl.rcParams['lines.linewidth'] = 2
 pl.rcParams['font.size'] = 14
@@ -50,7 +49,7 @@ def verify_time(prob = None, D = True, tf = 1, k = 10, kmin = 0, order = 1, save
         if D:
             u = p.get_sol(abs(xa)*Nx + 1, Nx + 1, dx, xa)
             errs.append(np.linalg.norm(u - ref, 2)*L2_fac)
-            f = -p.get_flux()
+            f = p.get_flux()
             errs_f.append(np.linalg.norm(f - ref_f, 2)*L2_fac_intf)
         else:
             u = p.get_sol(Nx + 1, Nx + 1, dx, 0)
@@ -58,7 +57,6 @@ def verify_time(prob = None, D = True, tf = 1, k = 10, kmin = 0, order = 1, save
     for i in range(k-1-kmin):
         print(np.log2(errs[i]/errs[i+1]))
     if D:
-        print('flux')
         for i in range(k-1-kmin):
             print(np.log2(errs_f[i]/errs_f[i+1]))
      
@@ -96,7 +94,7 @@ def verify_space(prob = None, D = True, tf = 1, N_steps = 100, k = 10, kmin = 0,
         if D:
             u = p.get_sol(abs(xa)*Nx + 1, Nx + 1, dxs[-1], xa)
             errs.append(np.linalg.norm(u - ref, 2)*L2_fac)
-            f = -p.get_flux()
+            f = p.get_flux()
             errs_f.append(np.linalg.norm(f - ref_f, 2)*L2_fac_intf)
         else:
             u = p.get_sol(Nx + 1, Nx + 1, dxs[-1], 0)

src/heat_DN/fenics_python/heat_fenics_grad.py
@@ -71,10 +71,10 @@ class Problem_heat_N(Problem_heat):
         dt = tf/n_steps
         for i, t in enumerate(np.linspace(0, tf, n_steps + 1)[:-1]):
             self.get_ex_flux(t) # get solution for flux into self.usol
-            flux_old = -np.copy(self.get_u_gamma(self.usol))
+            flux_old = np.copy(self.get_u_gamma(self.usol))
             
             self.get_ex_flux(t + dt) # get solution for flux into self.usol
-            flux_new = -np.copy(self.get_u_gamma(self.usol))
+            flux_new = np.copy(self.get_u_gamma(self.usol))
             
             self.do_step(dt, flux_old, flux_new)
     
@@ -128,7 +128,3 @@ if __name__ == '__main__':
     pp = {'tf': 1., **get_parameters(), 'gridsize': 16, 'xa': -2, 'xb': 1, 'theta': 0.5}
 #    verify_self_time(solve_WR = solver, savefig = savefig, k = 6, order = 1, **pp)
 #    verify_self_time(solve_WR = solver, savefig = savefig, k = 6, order = 2, **pp)
-    
-    pp = {'xa': -1, 'xb': 1, 'alpha': 1, 'lambda_diff': 0.01, 'gridsize': 32}
-    print(solver(1., 100, 100, 100, 1e-6, 0.5, **pp))
-#    solve_WR(tf, N1, N2, maxiter, TOL, th, Nx = None, Ny = None, dx = None, **kwargs):

src/heat_DN/fenics_python/verification_D_N.py
@@ -9,7 +9,6 @@ Created on Wed May 20 16:43:51 2020
 import numpy as np
 import pylab as pl
 from heat_fenics import Problem_heat
-from scipy.interpolate import interp1d
 
 pl.rcParams['lines.linewidth'] = 2
 pl.rcParams['font.size'] = 18
@@ -55,7 +54,7 @@ def verify_time(prob = None, D = True, tf = 1, k = 10, kmin = 0, order = 1, save
         if D:
             u = p.get_sol(abs(xa)*Nx + 1, Nx + 1, dx, xa)
             errs.append(np.linalg.norm(u - ref, 2)*L2_fac)
-            f = -p.get_flux()
+            f = p.get_flux()
             errs_f.append(np.linalg.norm(f - ref_f, 2)*L2_fac_intf)
         else:
             u = p.get_sol(Nx + 1, Nx + 1, dx, 0)
@@ -101,7 +100,7 @@ def verify_space(prob = None, D = True, tf = 1, N_steps = 100, k = 10, kmin = 0,
         if D:
             u = p.get_sol(abs(xa)*Nx + 1, Nx + 1, dxs[-1], xa)
             errs.append(np.linalg.norm(u - ref, 2)*L2_fac)
-            f = -p.get_flux()
+            f = p.get_flux()
             errs_f.append(np.linalg.norm(f - ref_f, 2)*L2_fac_intf)
         else:
             u = p.get_sol(Nx + 1, Nx + 1, dxs[-1], 0)